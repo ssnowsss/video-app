2334972a6be5cf6e539a2e168aa5058a8ca0a49e
eslam_samy <102614083+ssnowsss@users.noreply.github.com>
2023-02-23 15:32:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     next()
 })
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "https://billowy-hospital.surge.sh"
 }))
 app.use(express.json())
 // moudules
